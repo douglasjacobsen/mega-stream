81bbf6a754d5b7d92e0ec1af710ae47e073be829
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-03-30 14:38:36 +0100

    Make len value pass by reference

--- alloc.c
+++ alloc.c
@@ -3,9 +3,9 @@
 
 #define ALIGNMENT 2*1024*1024
 
-double * alloc(int len) {
-  double * p = (double *)aligned_alloc(ALIGNMENT, sizeof(double)*len);
-  printf("Allocated %p size %zu\n", p, sizeof(double)*len);
+double * alloc(int *len) {
+  double * p = (double *)aligned_alloc(ALIGNMENT, sizeof(double)*(*len));
+  printf("Allocated %p size %zu. len was %d\n", p, sizeof(double)*(*len), (*len));
   return p;
 }
 
