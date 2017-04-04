0c526182010b010979a74abcd460e2c0d6a1cff8
Tom Deakin <tom.deakin@bristol.ac.uk>
2017-04-04 11:15:45 +0100

    Add license to C allocation file

--- alloc.c
+++ alloc.c
@@ -1,3 +1,28 @@
+/*
+
+  Copyright 2016 Tom Deakin, University of Bristol
+
+  This file is part of mega-stream.
+
+  mega-stream is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  mega-stream is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with mega-stream.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  This aims to investigate the limiting factor for a simple kernel, in particular
+  where bandwidth limits not to be reached, and latency becomes a dominating factor.
+
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
